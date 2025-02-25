df3a866834dace752e92d1088ae72d3355b2ebc6
Yanay Zabary <yanay.tsabary@gmail.com>
2025-02-25 22:51:36 +0200

    ad logs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 // server.js
-import express from "express";
+import express, {
+  Request as ExReq,
+  Response as ExRes,
+  NextFunction,
+} from "express";
 import logRoutes from "./routes/logs.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req: ExReq, res: ExRes, next: NextFunction) => {
+  let logString = `New request: ${req.method} ${req.path}`;
+  console.log(logString);
+  next(); // Pass control to the next middleware or route handler
+});
 // Route: Return Spark logs as JSON
 app.use("/api/v1/logs", logRoutes);
 
